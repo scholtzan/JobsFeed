9166dfc9f19a66964ff8658ebd7e8e2ff05c1e78
Anna Scholtz <anna@scholtzan.net>
2024-07-20 14:39:07 -0700

    Create migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,18 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit'
 import * as dotenv from 'dotenv';
 dotenv.config();
 const { DATABASE_URL } = process.env;
 if (!DATABASE_URL) {
 	throw new Error('No url');
 }
-export default {
-	schema: './src/lib/server/database/schemas.ts',
+
+export default defineConfig({
+    schema: './src/lib/server/database/schemas.ts',
 	out: './src/lib/server/database/migrations',
-	driver: 'pg',
-	dbCredentials: {
-		connectionString: DATABASE_URL
-	}
-} satisfies Config;
+    dialect: 'postgresql',
+    dbCredentials: {
+        url: DATABASE_URL,
+    },
+    verbose: true,
+    strict: true,
+})
